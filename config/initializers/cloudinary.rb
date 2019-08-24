cc3956881ce7e7d576a0fa25e20fbff02ebe340c
Robin Klaus <rmklaus12@gmail.com>
2019-08-24 14:40:33 +1000

    config files for heroku

---
+++ config/initializers/cloudinary.rb
@@ -0,0 +1,7 @@
+Cloudinary.config do |config|
+  config.cloud_name = 'charmdev'
+  config.api_key = '758286288159677'
+  config.api_secret = 'bTDM9IaUZswaiXCZ8OvdsGRGJUk'
+  config.secure = true
+  config.cdn_subdomain = true
+end
